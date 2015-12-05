402700437a689998d6349ae7c9d486a665f8e8e9
Harry Park <imharrypark@gmail.com>
2015-12-04 21:59:19 -0500

    create canvas element onto html

---
+++ javascript/game.js
@@ -0,0 +1,6 @@
+// Create Canvas
+var canvas = document.createElement("canvas");
+var context = canvas.getContext("2d");
+canvas.width = 500;
+canvas.height = 500;
+document.body.appendChild(canvas);
