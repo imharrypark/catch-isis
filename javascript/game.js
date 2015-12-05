be180585dfa57d5918562759530a8bc4dd2b0295
Harry Park <imharrypark@gmail.com>
2015-12-04 22:24:45 -0500

    create game objects

--- javascript/game.js
+++ javascript/game.js
@@ -5,10 +5,22 @@ canvas.width = 500;
 canvas.height = 500;
 document.body.appendChild(canvas);
 
-// Create Images
+// Include Background Image
 var isBgReady = false;
 var bgImage = new Image();
 bgImage.onload = function(){
   isBgReady = true;
 };
 bgImage.src = "images/background.jpg";
+
+// Include Game Objects
+var captain = {
+  x: 0,
+  y: 0,
+  speed: 230;
+}
+var isis = {
+  x: 0,
+  y: 0
+};
+var numberCaught = 0;
