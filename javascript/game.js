1af5e72b150282544a329500c8a6976c981a2194
Harry Park <imharrypark@gmail.com>
2015-12-04 22:18:16 -0500

    include background image

--- javascript/game.js
+++ javascript/game.js
@@ -4,3 +4,11 @@ var context = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 500;
 document.body.appendChild(canvas);
+
+// Create Images
+var isBgReady = false;
+var bgImage = new Image();
+bgImage.onload = function(){
+  isBgReady = true;
+};
+bgImage.src = "images/background.jpg";
